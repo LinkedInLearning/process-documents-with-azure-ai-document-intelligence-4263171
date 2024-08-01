cd2d39068d386e52717c70b8d66f565d839a174e
zzulueta <41460714+zzulueta@users.noreply.github.com>
2024-08-01 16:07:04 +0000

    solution python file

--- exercisedata/instructions.md
+++ exercisedata/instructions.md
@@ -1,12 +1,19 @@
 ## Exercise Instructions
-1. Create a custom model for the Covid Consent forms
-2. The custom model must capture the following information:
+1. Create an Azure AI Document Intelligence Resource. 
+2. Create an Azure Storage Account and store the trainingdata in a Blob Container.
+2. Create a custom model project for the Covid Consent forms
+3. You will label the documents to capture the following information:
    a. Full Name - Last, Middle, First Names
    b. Date of Birth
    c. Age
    d. Full Address - Home Address, City, State, Zip, and Phone Number
    e. Date of Signature
-   f. If the person received a vaccine, the manufacturer of the received and the received date
-3. Train a custom neural model using the files inside training data folder.
-4. Extract the fields of the test data using REST API
-5. Print out the fields using Python code
+   f. If the person received a vaccine, the manufacturer of the vaccine and the received date
+4. Train a custom neural model using the files inside trainingdata folder.
+5. Verify the results of the model using the test form inside the testdata folder inside the Studio
+6. Extract the fields using REST API
+7. Print out the fields using Python code
+
+Setup time: 15 mins
+Training time: 20-60 mins 
+Tip: Selection marks for different scenarios.
---
+++ exercisedata/solution/solution.py
@@ -0,0 +1,58 @@
+def analyze_customfields(result):
+    if result.documents:
+        for document in result.documents:
+            if document.fields:
+                if document.fields["LastName"]: print(f"LastName: {document.fields['LastName'].value_string}")
+                if document.fields["FirstName"]: print(f"FirstName: {document.fields['FirstName'].value_string}")
+                if document.fields["MiddleName"]: print(f"MiddleName: {document.fields['MiddleName'].value_string}")
+                if document.fields["DateOfBirth"]: print(f"DateOfBirth: {document.fields['DateOfBirth'].value_date}")
+                if document.fields["Age"]: print(f"Age: {document.fields['Age'].value_number}")
+                if document.fields["Male"]: print(f"Male: {document.fields['Male'].value_selection_mark}")
+                if document.fields["Female"]: print(f"Female: {document.fields['Female'].value_selection_mark}")
+                if document.fields["Other"]: print(f"Other: {document.fields['Other'].value_selection_mark}")
+                if document.fields["StreetAddress"]: print(f"StreetAddress: {document.fields['StreetAddress'].value_string}")
+                if document.fields["City"]: print(f"City: {document.fields['City'].value_string}")
+                if document.fields["State"]: print(f"State: {document.fields['State'].value_string}")
+                if document.fields["Zip"]: print(f"Zip: {document.fields['Zip'].value_string}")
+                if document.fields["PhoneNumber"]: print(f"PhoneNumber: {document.fields['State'].value_string}")
+                if document.fields["DateOfSignature"]: print(f"DateOfSignature: {document.fields['DateOfSignature'].value_date}")
+                if document.fields["ReceivedVaccineYes"]: print(f"ReceivedVaccineYes: {document.fields['ReceivedVaccineYes'].value_selection_mark}")
+                if document.fields["ReceivedVaccineNo"]: print(f"ReceivedVaccineNo: {document.fields['ReceivedVaccineNo'].value_selection_mark}")
+                if document.fields["VaccineManufacturer"]: print(f"VaccineManufacturer: {document.fields['VaccineManufacturer'].value_string}")
+                if document.fields["ReceivedDate"]: print(f"ReceivedDate: {document.fields['ReceivedDate'].value_string}")
+
+def analyze_document():
+    from dotenv import find_dotenv, load_dotenv
+
+    # Load environment variables from .env file
+    load_dotenv(find_dotenv())
+
+    import os
+    # get parameters from environment variables
+    endpoint = os.environ["AZURE_DOCUMENT_INTELLIGENCE_ENDPOINT"]
+    key = os.environ["AZURE_DOCUMENT_INTELLIGENCE_KEY"]
+
+    from azure.ai.documentintelligence import DocumentIntelligenceClient    
+    from azure.core.credentials import AzureKeyCredential
+
+    # use api key to create credential
+    credential=AzureKeyCredential(key)
+
+    document_analysis_client = DocumentIntelligenceClient(endpoint, credential)
+    
+    from azure.ai.documentintelligence.models import AnalyzeDocumentRequest, AnalyzeResult
+    from azure.ai.documentintelligence.models import DocumentAnalysisFeature
+    
+    # Analyze a document at a URL:
+    docUrl = "https://ziggyzuluetastorage01.blob.core.windows.net/documents/04 - consent-form-test-document.pdf"
+    poller = document_analysis_client.begin_analyze_document(
+        "linkedinexercise02", #enter custom model id here
+        AnalyzeDocumentRequest(url_source=docUrl)
+    )  
+
+    result: AnalyzeResult = poller.result()    
+    analyze_customfields(result)
+
+
+if __name__ == "__main__":
+    analyze_document()
